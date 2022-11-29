d6493124f942fff2efe67f4cd3d350c4c9f135cb
Alice <alice668149@gmail.com>
2022-11-29 20:47:14 +0800

    env: add server connection

---
+++ backend/src/server.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+
+require('dotenv').config();
+const app = express();
+
+// init middleware
+app.use(cors());
+app.use(express.json());
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next();
+});
+
+const port = process.env.PORT || 4000;
+
+if (!process.env.MONGO_URL) {
+    console.error('Missing MONGO_URL!');
+    process.exit(1);
+} else {
+    mongoose.connect(process.env.MONGO_URL, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+    });
+}
+
+// routes(app);
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}.`);
+});
