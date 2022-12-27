c7fe08bf8479b169df22404a64e4f15d74a90da0
guan404ming <guanmingchiu@gmail.com>
2022-12-28 01:27:32 +0800

    feat: add header

--- frontend/src/components/MyHeader.js
+++ frontend/src/components/MyHeader.js
@@ -1,23 +1,42 @@
 import React from 'react';
 import Input from 'antd/es/input/Input';
-import { Button } from 'antd';
+import { Button, Typography } from 'antd';
+import styled from 'styled-components';
 
-// const Wrapper = styled(Menu)`
-//     .ant-menu-item-selected {
-//         background-color: #FFF1B8 !important;
-//         color: #FAAD14;
-//     }
-// }`;
+const { Title } = Typography;
+
+const Header = styled.header`
+    display: flex;
+    height: 64px;
+    padding: 16px 16px;
+    justify-content: center;
+}`;
+
+const Container = styled.div`
+    display: flex;
+}`;
+
+const inputStyle = {
+    width: '500px',
+    margin: '0 240px',
+};
+
+const buttonStyle = {
+    background: '#FAAD14',
+    borderRadius: '16px',
+};
 
 function MyHeader() {
     return (
-        <div>
-            <h1>Bus Error</h1>
-            <Input />
-            <Button>問題</Button>
-            <Button>標籤</Button>
-            <Button>登入 / 註冊</Button>
-        </div>
+        <Header>
+            <Title level={3} style={{ width: '220px' }}>Bus Error</Title>
+            <Input style={inputStyle} placeholder="搜尋" />
+            <Container>
+                <Button type="text">問題</Button>
+                <Button type="text">標籤</Button>
+                <Button type="primary" style={buttonStyle}>登入 / 註冊</Button>
+            </Container>
+        </Header>
     );
 }
 
