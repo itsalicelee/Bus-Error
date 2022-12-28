34547b58e00b391bbd23f83f2b342e7e924e36cb
guan404ming <guanmingchiu@gmail.com>
2022-12-28 23:31:50 +0800

    fix: item select error

--- frontend/src/components/MyMenu.js
+++ frontend/src/components/MyMenu.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { Menu } from 'antd';
 import styled from 'styled-components';
 
-const Wrapper = styled(Menu)`
+const Container = styled(Menu)`
+    background-color: #FAFAFA;
     .ant-menu-item-selected {
         background-color: #FCF4E0 !important;
         color: #FAAD14;
@@ -11,38 +12,35 @@ const Wrapper = styled(Menu)`
 }`;
 
 const followingTags = [
-    { label: '追蹤中的主標籤', type: 'group' },
-    { label: 'C / UNIX', key: 'item-1' },
-    { label: 'JS / React', key: 'item-2' },
-    { label: 'Python', key: 'item-3' },
-];
-
-const popularTags = [
-    { label: '熱門的主標籤', type: 'group' },
-    { label: 'JS / Vue.js', key: 'item-1' },
-    { label: 'JS / Angular', key: 'item-2' },
-    { label: '軟體 / Adobe XD', key: 'item-3' },
+    {
+        type: 'group',
+        label: '追蹤中的主標籤',
+        children: [
+            { label: 'C / UNIX', key: 'item-1' },
+            { label: 'JS / React', key: 'item-2' },
+            { label: 'Python', key: 'item-3' },
+        ],
+    },
+    {
+        type: 'group',
+        label: '熱門的主標籤',
+        children: [
+            { label: 'JS / Vue.js', key: 'item-4' },
+            { label: 'JS / Angular', key: 'item-5' },
+            { label: 'UI / Adobe XD', key: 'item-6' },
+        ],
+    },
 ];
 
 function MyMenu() {
     return (
-        <div>
-            <Wrapper
-                items={followingTags}
-                style={{ width: 224 }}
-                defaultSelectedKeys={['1']}
-                defaultOpenKeys={['sub1']}
-                mode="inline"
-            />
-
-            <Wrapper
-                items={popularTags}
-                style={{ width: 224 }}
-                defaultSelectedKeys={['1']}
-                defaultOpenKeys={['sub1']}
-                mode="inline"
-            />
-        </div>
+        <Container
+            items={followingTags}
+            style={{ width: '200px' }}
+            defaultSelectedKeys={['1']}
+            defaultOpenKeys={['sub1']}
+            mode="inline"
+        />
     );
 }
 
