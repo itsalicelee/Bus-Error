0342a98f481a187cf9d04dd0c7022b32f9698bf7
Hon Chi Kit <kinoras@kinoras.com>
2022-12-29 23:19:19 +0800

    env: config color for frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import { gold } from '@ant-design/colors';
+import { ConfigProvider, theme } from 'antd';
+
 import './App.css';
 import Header from './container/Header';
 import Body from './container/Body';
 
+const colorAlgorithm = theme.defaultAlgorithm;
+// const colorAlgorithm = theme.darkAlgorithm;
+
 function App() {
     return (
-        <>
+        <ConfigProvider
+            theme={{
+                token: { colorPrimary: gold.primary },
+                algorithm: colorAlgorithm,
+            }}
+        >
             <Header />
             <Body />
-        </>
+        </ConfigProvider>
     );
 }
 
